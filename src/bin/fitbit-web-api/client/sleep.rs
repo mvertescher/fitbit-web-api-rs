6a00eaa399c4a9d4b32e9a799617f418f30c4437
Josh Kuhn <deontologician@gmail.com>
2021-09-25 17:22:37 -0700

    Implement sleep list

--- src/bin/fitbit-web-api/client/sleep.rs
+++ src/bin/fitbit-web-api/client/sleep.rs
@@ -1,7 +1,10 @@
 //! Client for the sleep endpoints
 
+use std::error::Error;
+
 use super::get;
 
+use chrono::prelude::*;
 use fitbit_web_api::*;
 
 pub(crate) async fn get_goal() {
@@ -12,9 +15,22 @@ pub(crate) async fn get_goal() {
 }
 
 pub(crate) async fn get_list() {
-    let url = sleep::list::url(UserId::Current);
-    let _body = get(url).await;
-    // TODO: Parse response
+    let url = sleep::list::url(
+        UserId::Current,
+        100,
+        0,
+        Utc::now().with_timezone(&Local) - chrono::Duration::days(8),
+    );
+    let body = get(url).await;
+    let response: sleep::list::get::Response = match serde_json::from_str(&body) {
+        Ok(r) => r,
+        Err(e) => {
+            println!("{}", e);
+            println!("Error at: {}", &body[e.column()-20 ..= e.column()+20]);
+            return
+        },
+    };
+    println!("{:#?}", response);
 }
 
 pub(crate) async fn get_log() {
